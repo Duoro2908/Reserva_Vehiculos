3cf8872e01a527f016c98704612cd560ad82f0e9
Duoro2908 <elvinalexandermendezcruz05@gmail.com>
2024-10-06 08:23:02 -0600

    primer cambio confirmado

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Reserva_Vehiculos
+{
+    class Program
+    {
+          public  static void Main(string[] args)
+          {
+
+            string modelo = "Bugatti Cayse";
+            int precio = 20000;
+            int dias = 2;
+            double descuento = 10.0; 
+
+            Console.WriteLine($"Modelo predeterminado: {modelo}");
+            Console.WriteLine($"Precio predeterminado: {precio}");
+            Console.WriteLine($"Días de reserva predeterminados: {dias}");
+            Console.WriteLine($"Descuento predeterminado: {descuento}%");
+            Console.WriteLine("El costo total de la reserva con un descuento del 10% es:36,000.");
+            Console.WriteLine("***********************************");
+
+
+            Console.WriteLine("Bienvenidos a nuestro Concesionario de vehiculos!!!!!!!");
+
+            Console.WriteLine("Lo anterior era un ejemplo de como usar nuestro sistema");
+        
+
+            Console.WriteLine("Ingrese el modelo del vehículo que desee por favor :");
+            modelo = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el precio del vehículo:");
+            precio = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la cantidad de dias que quiere la reserva:");
+            dias = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el porcentaje de descuento de el dia de hoy:");
+            descuento = double.Parse(Console.ReadLine());
+
+            Vehiculoo miVehiculo = new Vehiculoo(modelo, precio);
+
+            miVehiculo.Mostrar_Info_Vehiculo();
+
+            double costoTotal = Calculo_Reserva_Vehiculo.Calcular_Costo_Total(miVehiculo, dias, descuento);
+
+            Console.WriteLine($"El costo total de la reserva con un descuento del {descuento}% es: {costoTotal}.");
+
+            Console.WriteLine("___Gracias por Preferirnos estaremos esperandolo nuevamente___");
+        }
+    }
+ }
---
+++ Vehiculoo.cs
@@ -0,0 +1,53 @@
+﻿using Reserva_Vehiculos;
+using System.Runtime.CompilerServices;
+
+namespace Reserva_Vehiculos
+{
+    public class Vehiculoo
+    {
+        public string Modelo { get; set; }
+
+        public int Precio { get; set; }
+
+        public Vehiculoo(string modelo, int precio)
+        {
+            Modelo = modelo;
+            Precio = precio;
+        }
+
+        public class Descuento
+        {
+            public double Porcentaje_des {  get; set; }
+            public Descuento(double Porcentaje)
+            {
+                Porcentaje_des = Porcentaje;
+            }
+
+            public static double Hacer_Desc(double precio_original, double descuento_porcentaje)
+            {
+                return precio_original - (precio_original * descuento_porcentaje / 100);
+            }
+        }
+    }
+}
+public static class Vehiculoo_Ext
+{ 
+    public static void Mostrar_Info_Vehiculo(this Vehiculoo mivehiculo)
+    {
+        Console.WriteLine($"Modelo_del_vehiculo: {mivehiculo.Modelo}");
+        Console.WriteLine($"Precio_del_vehiculo: {mivehiculo.Precio}");
+    }
+}
+//El this es lo que lo hace un metodo de extension ya que Vehiculo es de mi primer clase
+//clase estatica para calcular el valor de la reserva
+
+public static class Calculo_Reserva_Vehiculo
+{
+    public static double Calcular_Costo_Total(Vehiculoo mivehiculo, int dias, double descuento)
+    {
+        double costo = mivehiculo.Precio * dias;
+        costo = Vehiculoo.Descuento.Hacer_Desc( costo, descuento ); 
+        return costo;
+    }
+
+}
